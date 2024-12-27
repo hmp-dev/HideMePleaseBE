f2154379e57defa47c07632fb578857fa6ba7223
yatanvesh <yatan.vesh@gmail.com>
2024-12-27 09:55:51 +0530

    fix: locale date

--- cms/extensions/directus-extension-locale-date/src/display.vue
+++ cms/extensions/directus-extension-locale-date/src/display.vue
@@ -1,5 +1,7 @@
 <template>
-	<div>{{ new Date(value).toLocaleString() }}</div>
+	<div>
+		{{ formatted }}
+	</div>
 </template>
 
 <script lang="ts">
@@ -11,6 +13,24 @@ export default defineComponent({
 			type: String,
 			default: null,
 		},
+	},
+	inject: ['value'],
+	setup(props) {
+		const date = new Date(
+			props.value.includes('Z') ? props.value : props.value + 'Z',
+		);
+
+		function formatDateToYYYYMMDDHHMM(date) {
+			const year = date.getFullYear();
+			const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
+			const day = String(date.getDate()).padStart(2, '0');
+			const hours = String(date.getHours()).padStart(2, '0');
+			const minutes = String(date.getMinutes()).padStart(2, '0');
+
+			return `${year}/${month}/${day} ${hours}:${minutes}`;
+		}
+
+		return { formatted: formatDateToYYYYMMDDHHMM(date) };
 	},
 });
 </script>
