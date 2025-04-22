e2cf40782a4467b234f99038b5ff092c0004f51a
takout <takout@takoutui-Macmini.local>
2025-04-22 18:08:47 +0900

    avax

--- backend/src/modules/web3/web3.constants.ts
+++ backend/src/modules/web3/web3.constants.ts
@@ -7,18 +7,21 @@ export const SupportedChainsList = [
 	SupportedChains.POLYGON,
 	SupportedChains.KLAYTN,
 	SupportedChains.SOLANA,
+	SupportedChains.AVALANCHE,
 ];
 
 export const SupportedChainMapping = {
 	[SupportedChains.ETHEREUM]: EvmChain.ETHEREUM,
 	[SupportedChains.POLYGON]: EvmChain.POLYGON,
 	[SupportedChains.MUMBAI]: EvmChain.MUMBAI,
+	[SupportedChains.AVALANCHE]: EvmChain.AVALANCHE,
 } satisfies Record<string, EvmChain>;
 
 export const SupportedChainCovalentMapping = {
 	[SupportedChains.ETHEREUM]: 'eth-mainnet',
 	[SupportedChains.POLYGON]: 'matic-mainnet',
 	[SupportedChains.MUMBAI]: 'matic-mumbai',
+	[SupportedChains.AVALANCHE]: 'avalanche-mainnet',
 } satisfies Record<string, Chain>;
 
 export const ChainToSymbolMapping = {
@@ -27,10 +30,12 @@ export const ChainToSymbolMapping = {
 	[SupportedChains.MUMBAI]: 'Matic',
 	[SupportedChains.SOLANA]: 'Sol',
 	[SupportedChains.KLAYTN]: 'Klay',
+	[SupportedChains.AVALANCHE]: 'Avax',
 };
 
 export const SupportedChainSimpleHashMapping = {
 	[SupportedChains.ETHEREUM]: 'ethereum',
 	[SupportedChains.SOLANA]: 'solana',
 	[SupportedChains.POLYGON]: 'polygon',
+	[SupportedChains.AVALANCHE]: 'avalanche',
 };
